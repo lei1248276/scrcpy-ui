d6c2378f6ab356dc12753953b1dce1dfb9fa6bd7
Jaye <jaye1248276@gmail.com>
2024-08-03 22:19:22 +0800

    refactor: 简化UI操作

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,12 +1,5 @@
 <template>
   <div class="flex min-h-screen flex-col items-center justify-center gap-4 bg-[#1e1e20]">
-    <Input
-      type="text"
-      placeholder="添加ip地址"
-      class="w-[200px]"
-      @keyup.enter="onScrcpy(ip = $event.target.value)"
-    />
-
     <Popover v-model:open="open">
       <PopoverTrigger as-child>
         <Button
@@ -15,9 +8,7 @@
           :aria-expanded="open"
           class="w-[200px] justify-between"
         >
-          {{ ip
-            ? ips.find((v) => v.value === ip)?.label
-            : "选择ip地址..." }}
+          {{ ip || "USB连接" }}
           <CaretSortIcon class="ml-2 size-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
@@ -25,32 +16,28 @@
         <Command>
           <CommandInput
             class="h-9"
-            placeholder="搜索ip地址..."
+            placeholder="新增/搜索IP地址"
+            @keyup.enter="onScrcpy(ip = $event.target.value); open = false"
           />
           <CommandEmpty>没有ip地址....</CommandEmpty>
           <CommandList>
             <CommandGroup>
               <CommandItem
                 v-for="(v, i) in ips"
-                :key="v.value"
-                :value="v.value"
-                @select="(ev) => {
-                  if (typeof ev.detail.value === 'string') {
-                    ip = ev.detail.value
-                  }
-                  open = false
-                }"
+                :key="v"
+                :value="v"
+                @select="({ detail: { value }}) => { typeof value === 'string' && (ip = value === ip ? '' : value); open = false }"
               >
-                {{ v.label }}
+                {{ v }}
                 <CheckIcon
                   :class="cn(
                     'ml-auto h-4 w-4',
-                    ip === v.value ? 'opacity-100' : 'opacity-0',
+                    ip === v ? 'opacity-100' : 'opacity-0',
                   )"
                 />
                 <Cross2Icon
                   class="ml-auto size-4 cursor-pointer"
-                  @click.stop="ips.splice(i, 1); saveIps()"
+                  @click.stop="ips.splice(i, 1); saveIps(); ip === v && (ip = '')"
                 />
               </CommandItem>
             </CommandGroup>
@@ -59,24 +46,21 @@
       </PopoverContent>
     </Popover>
 
-    <Button
-      variant="secondary"
-      @click="onScrcpy(ip)"
-    >
-      Scrcpy TCP Start
-    </Button>
-    <Button
-      variant="secondary"
-      @click="onScrcpy('')"
-    >
-      Scrcpy USB Start
-    </Button>
-    <Button
-      variant="destructive"
-      @click="onScrcpyKill"
-    >
-      Scrcpy Kill
-    </Button>
+    <div class="flex items-center gap-x-2">
+      <Button
+        variant="secondary"
+        @click="onScrcpy(ip)"
+      >
+        Scrcpy Start
+      </Button>
+
+      <Button
+        variant="destructive"
+        @click="onScrcpyKill"
+      >
+        Scrcpy Kill
+      </Button>
+    </div>
   </div>
 </template>
 
@@ -84,37 +68,35 @@
 import { CaretSortIcon, CheckIcon, Cross2Icon } from '@radix-icons/vue'
 import { cn } from '@renderer/utils/lib/utils'
 
-type IP = {
-  label: string
-  value: string
-}
-const ips = reactive<IP[]>([])
-
 const open = ref(false)
+const ips = reactive<string[]>([])
 const ip = ref('')
 
 onMounted(() => {
   try {
     const _ips = localStorage.getItem('ips')
-    console.log('%c🚀 ~ file: App.vue:102 ~ onMounted ~ _ips:', 'color:#a8c7fa', _ips, ip.value)
-    if (_ips) {
-      ips.push(...JSON.parse(_ips))
-    }
+    console.log('%c🚀 ~ file: App.vue:102 ~ onMounted ~ _ips:', 'color:#a8c7fa', _ips)
+    if (!_ips) return
+
+    const parsedIps = JSON.parse(_ips) as string[]
+    parsedIps.some(v => typeof v !== 'string')
+      ? localStorage.removeItem('ips')
+      : ips.push(...JSON.parse(_ips))
   } catch (err) {
     console.error('🚀 ~ file: App.vue:107 ~ onMounted ~ err:', err)
   }
 })
 
 const saveIps = () => {
-  localStorage.setItem('ips', JSON.stringify(ips))
-  window.electron.ipcRenderer.send('setStoreIps', ips.map((v) => v.value).filter(Boolean))
+  const _ips = ips.filter(Boolean)
+  localStorage.setItem('ips', JSON.stringify(_ips))
+  window.electron.ipcRenderer.send('setStoreIps', _ips)
 }
 
 const onScrcpy = (ip: string) => {
   console.log('%c🚀 ~ file: App.vue:92 ~ onScrcpy ~ ip:', 'color:#a8c7fa', ips, ip)
-  if (ips.findIndex((v) => v.value === ip) === -1) {
-    const res = { label: ip, value: ip }
-    ips.push(res)
+  if (ip && !ips.includes(ip)) {
+    ips.push(ip)
     saveIps()
   }
   window.electron.ipcRenderer.send('scrcpy', ip)
